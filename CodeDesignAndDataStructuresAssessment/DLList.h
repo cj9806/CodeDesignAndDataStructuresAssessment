62a2b8b5cbb3cd79393384607ae10f268912403e
caleb mccombs <cthepyro@gmail.com>
2021-02-17 18:01:44 -0800

    worked on DLList.h possibly finished, will confirm later

--- CodeDesignAndDataStructuresAssessment/DLList.h
+++ CodeDesignAndDataStructuresAssessment/DLList.h
@@ -22,6 +22,7 @@ public:
 			temp->next = head;
 
 			head = temp;
+			count++;
 		}
 		else {
 			Node* temp = new Node;
@@ -31,12 +32,15 @@ public:
 
 			head = temp;
 			tail = temp;
+			count = 0;
 		}
+		
 	}
 	void pop_front() {
 		Node* temp = head;
 		head = head->next;
 		delete temp;
+		count--;
 		if (head!= nullptr)
 		{
 			head->prev = nullptr;
@@ -49,29 +53,124 @@ public:
 
 	//insert node at back of list
 	//delete node at back of list
-	void insertBack(struct Node** tailRef, int newData);
-	void deleteBack();
+	void push_back(const T& newData) {
+		if (tail != nullptr) {
+			Node* temp = new Node;
+			temp->data = newData;
+			temp->next = nullptr;
+			temp->prev = tail;
+
+			tail = temp;
+			count++;
+		}
+		else {
+			Node* temp = new Node;
+			temp->data = newData;
+			temp->prev = nullptr;
+			temp->next = nullptr;
+
+			head = temp;
+			tail = temp;
+			count = 0;
+		}
+	}
+	void pop_back() {
+		Node* temp = tail;
+		tail = tail->prev;
+		delete temp;
+		count--;
+		if (tail != nullptr) {
+			tail->next = nullptr;
+		}
+		else {
+			head = nullptr;
+		}
+
+	}
 
 	//insert node at arbitary location of list
 	//delete nose at arbitary location of list
-	void insertMid(struct Node* prevNode, int newData);
-	void deleteMid();
+
+	//list index will assume size = number of nodes -1 because of index of 0
+	//will add to tail of list if out of range
+	void push_mid(const T& newData, int listIndex) {
+		if (listIndex > count&& head != nullptr) {
+			Node* temp = new Node;
+			temp->data = newData;
+			temp->next = nullptr;
+			temp->prev = tail;
+
+			tail = temp;
+			count++;
+		}
+		else if (head != nullptr) {
+			Node* seed = head;
+			for (int i = 0; i < listIndex; i++;) {
+				seed = seed->next;
+			}
+			Node* temp = new Node;
+			temp->data = newData;
+			temp->prev = seed;
+			temp->next = seed->next;
+			seed->next = temp;
+			seed->next->prev = seed;
+			count++;
+		}
+		else {
+			Node* temp = new Node;
+			temp->data = newData;
+			temp->prev = nullptr;
+			temp->next = nullptr;
+
+			head = temp;
+			tail = temp;
+			count = 0;
+		}
+	}
+	//assumes you want to delete a node at a certing index with index starting at 0
+	//if indexis  out of range it will pop the tail
+	void pop_mid(int listIndex) {
+		if (listIndex > count && head != nullptr)
+		{
+			Node* temp = tail;
+			tail = tail->prev;
+			delete temp;
+			count--;
+			if (tail != nullptr) {
+				tail->next = nullptr;
+			}
+			else {
+				head = nullptr;
+			}
+		}
+		else if (head != nullptr)
+		{
+			Node* temp = head;
+			for (int i;i < listIndex;i++;)
+			{
+				temp = temp->next;
+			}
+			temp->next->prev = temp->prev;
+			temp->prev->next = temp->next;
+			delete temp;
+		}
+		
+	}
 
 	//return count of nodes in list
-	void countNodes();
+	int countNodes() {
+		return count+1;
+	}
 
 	//check if list is empty
-	void isListEmpty();
-
-	//returning first or last node in list
-	void returnFirst();
-	void returnLast();
-
-	//sort the list
-	void sortList();
+	bool isListEmpty() {
+		if (head == nullptr || tail == nullptr)
+			return true;
+		else return false;
+	}
 private:
 	Node* head;
 	Node* tail;
-	int count = 0;
+	int count = -1;
 };
 
